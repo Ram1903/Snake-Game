35d28d88f1d0b09eb40fc5ff8b43497499ff2559
unknown <ramagrawal1903@gmail.com>
2023-02-02 17:21:28 +0530

    first commit

---
+++ sankegame/main.java
@@ -0,0 +1,15 @@
+package sankegame;
+import java.awt.Color;
+import javax.swing.JFrame;
+public class main{
+    public static void main(String args[]){
+        JFrame frame=new JFrame("Snake Game");
+        frame.setBounds(10,10,905,700);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GamePannel panel=new GamePannel();
+        panel.setBackground(Color.GRAY);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
